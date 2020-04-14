565be95b0cf4cd3905a5d1ce0ad907e594d3352a
zelambe <zahralambe@wustl.edu>
2020-04-14 16:30:24 -0500

    Updated the GameBoard Class to include the start and pause screens

--- src/finalProject/gameBoard.java
+++ src/finalProject/gameBoard.java
@@ -1,18 +1,29 @@
 package finalProject;
 
+import java.awt.Font;
+
 import sedgewick.StdDraw;
 
 public class gameBoard {
-//
+
 	public void setup() {
 		//Necessary for drawing board and setting scales
 		StdDraw.setCanvasSize(850,850);
 		StdDraw.setScale(-1, 20);
 	}
 	
+	public void startScreen() {
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(10, 10, 5, 10);
+		Font tetrisFont = new Font("Arial",Font.BOLD,70);
+		Font subtitle = new Font("Arial",Font.CENTER_BASELINE,30);
+		StdDraw.setFont(tetrisFont);
+		StdDraw.text(10, 15, "Tetris");
+		StdDraw.setFont(subtitle);
+		StdDraw.text(10, 10, "Press Enter to Begin!");
+	}
 	
 	public void drawBoard() {
-		
 		
 		StdDraw.setPenColor(StdDraw.BLACK);
 		//Main Square
@@ -25,6 +36,10 @@ public class gameBoard {
 		StdDraw.rectangle(16.5, 15, 1.5, 5);
 	}
 
+	public void pauseScreen() {
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.rectangle(10, 10, 5, 10);
+	}
 
 	public void printLosingScreen() {
 		// TODO Auto-generated method stub
